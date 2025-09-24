5253f2a3936766814d4f3deb7ec357008cfe5aa4
NguyenQuocAnh-Git <nguyenquocanh211004@gmail.com>
2025-09-24 20:41:21 +0700

    Add animation and demo controller

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]public float mouseSensitivity = 100f;
+    public Transform playerBody;  // tham chiếu đến object Player
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        // Ẩn và khóa chuột vào giữa màn hình
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Lấy input chuột
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Xoay dọc (camera) - giới hạn từ -90 đến +90
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -60f, 70f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Xoay ngang (toàn bộ Player)
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private Animator animator;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponentInChildren<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        ApplyMovemet();
+        AnimatorController();
+    }
+
+    private void AnimatorController()
+    {
+        float zVelocity = Vector3.Dot(velocity, transform.forward);
+
+        animator.SetFloat("zVelocity", zVelocity);
+    }
+
+    private void ApplyMovemet()
+    {
+        // Kiểm tra có chạm đất không
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // giữ nhân vật dính mặt đất
+        }
+
+        // Nhận input di chuyển
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.x = move.x;
+        velocity.z = move.z;
+
+        Debug.Log(velocity.z);
+
+        // Nhảy
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Áp dụng trọng lực
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
